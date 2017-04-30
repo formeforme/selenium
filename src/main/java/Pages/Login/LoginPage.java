429915b7d605eadfc15b374a04d7e341941403a4
USER <namesurname.2016@yandex.ru>
2017-04-30 23:25:28 +0400

    test

--- src/main/java/Pages/Login/LoginPage.java
+++ src/main/java/Pages/Login/LoginPage.java
@@ -38,10 +38,10 @@ public class LoginPage extends State {
     }
 
     public void setUsername(String username){
-        usernameField.sendKeys(username);
+        usernameField.sendKeys(String.valueOf(username));
     }
     public void setPassword(String password){
-        passwordField.sendKeys(password);
+        passwordField.sendKeys(String.valueOf(password));
     }
     public String getUsername(){
         return usernameField.getText();
